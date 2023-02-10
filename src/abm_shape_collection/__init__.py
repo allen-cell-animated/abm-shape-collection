a70b9aa1002c3a249cb32ddabd0200da77861b49
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-02-10 11:42:48 -0500

    Move coefficients tasks to top level

--- src/abm_shape_collection/__init__.py
+++ src/abm_shape_collection/__init__.py
@@ -3,4 +3,6 @@ from .calculate_size_stats import calculate_size_stats
 from .compile_shape_modes import compile_shape_modes
 from .extract_shape_modes import extract_shape_modes
 from .fit_pca_model import fit_pca_model
+from .get_shape_coefficients import get_shape_coefficients
+from .make_voxels_array import make_voxels_array
 from .merge_shape_modes import merge_shape_modes

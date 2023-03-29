4651d511572128c145529d2e1465d4814fe13f51
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-03-29 19:01:41 -0400

    Add task decorator to init imports

--- src/abm_shape_collection/__init__.py
+++ src/abm_shape_collection/__init__.py
@@ -1,6 +1,3 @@
-import importlib
-import sys
-
 from prefect import task
 
 from .calculate_shape_stats import calculate_shape_stats
@@ -12,18 +9,11 @@ from .get_shape_coefficients import get_shape_coefficients
 from .make_voxels_array import make_voxels_array
 from .merge_shape_modes import merge_shape_modes
 
-TASK_MODULES = [
-    calculate_shape_stats,
-    calculate_size_stats,
-    compile_shape_modes,
-    extract_shape_modes,
-    fit_pca_model,
-    get_shape_coefficients,
-    make_voxels_array,
-    merge_shape_modes,
-]
-
-for task_module in TASK_MODULES:
-    MODULE_NAME = task_module.__name__
-    module = importlib.import_module(f".{MODULE_NAME}", package=__name__)
-    setattr(sys.modules[__name__], MODULE_NAME, task(getattr(module, MODULE_NAME)))
+calculate_shape_stats = task(calculate_shape_stats)
+calculate_size_stats = task(calculate_size_stats)
+compile_shape_modes = task(compile_shape_modes)
+extract_shape_modes = task(extract_shape_modes)
+fit_pca_model = task(fit_pca_model)
+get_shape_coefficients = task(get_shape_coefficients)
+make_voxels_array = task(make_voxels_array)
+merge_shape_modes = task(merge_shape_modes)

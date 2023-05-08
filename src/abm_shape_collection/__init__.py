a7702c4a37f3b5808879494dc456d5d164538278
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2023-05-08 13:59:57 -0400

    Add get shape properties task to init (#24)

--- src/abm_shape_collection/__init__.py
+++ src/abm_shape_collection/__init__.py
@@ -6,6 +6,7 @@ from .compile_shape_modes import compile_shape_modes
 from .extract_shape_modes import extract_shape_modes
 from .fit_pca_model import fit_pca_model
 from .get_shape_coefficients import get_shape_coefficients
+from .get_shape_properties import get_shape_properties
 from .make_voxels_array import make_voxels_array
 from .merge_shape_modes import merge_shape_modes
 
@@ -15,5 +16,6 @@ compile_shape_modes = task(compile_shape_modes)
 extract_shape_modes = task(extract_shape_modes)
 fit_pca_model = task(fit_pca_model)
 get_shape_coefficients = task(get_shape_coefficients)
+get_shape_properties = task(get_shape_properties)
 make_voxels_array = task(make_voxels_array)
 merge_shape_modes = task(merge_shape_modes)

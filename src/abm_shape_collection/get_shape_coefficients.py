ac63b74cd8af89847093b55065d7d782eae0b892
Jessica S. Yu <15913767+jessicasyu@users.noreply.github.com>
2023-07-11 11:16:50 -0400

    Update get shape coefficients task to output angle, center, and MSE (#32)

--- src/abm_shape_collection/get_shape_coefficients.py
+++ src/abm_shape_collection/get_shape_coefficients.py
@@ -6,8 +6,17 @@ def get_shape_coefficients(array: np.ndarray, reference: np.ndarray, lmax: int) 
     _, angle = shtools.align_image_2d(image=reference)
     aligned_array = shtools.apply_image_alignment_2d(array, angle).squeeze()
 
-    (coeffs, _), _ = shparam.get_shcoeffs(
+    (coeffs, reconstructed_array), (_, _, downsampled_array, _) = shparam.get_shcoeffs(
         image=aligned_array, lmax=lmax, compute_lcc=False, alignment_2d=False
     )
+
+    mse = shtools.get_reconstruction_error(downsampled_array, reconstructed_array)
+    center = np.argwhere(aligned_array == 1).mean(axis=0)
+
+    coeffs["angle"] = angle
+    coeffs["mse"] = mse
+    coeffs["center_z"] = center[0]
+    coeffs["center_y"] = center[1]
+    coeffs["center_x"] = center[2]
 
     return coeffs

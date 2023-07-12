46493eff111803f23e6e15c6327d2102224e8ab6
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-07-12 13:04:30 -0400

    Remove outdated shape mode tasks

--- src/abm_shape_collection/__init__.py
+++ src/abm_shape_collection/__init__.py
@@ -2,7 +2,6 @@ from prefect import task
 
 from .calculate_shape_stats import calculate_shape_stats
 from .calculate_size_stats import calculate_size_stats
-from .compile_shape_modes import compile_shape_modes
 from .construct_mesh_from_array import construct_mesh_from_array
 from .construct_mesh_from_coeffs import construct_mesh_from_coeffs
 from .construct_mesh_from_points import construct_mesh_from_points
@@ -12,11 +11,9 @@ from .fit_pca_model import fit_pca_model
 from .get_shape_coefficients import get_shape_coefficients
 from .get_shape_properties import get_shape_properties
 from .make_voxels_array import make_voxels_array
-from .merge_shape_modes import merge_shape_modes
 
 calculate_shape_stats = task(calculate_shape_stats)
 calculate_size_stats = task(calculate_size_stats)
-compile_shape_modes = task(compile_shape_modes)
 construct_mesh_from_array = task(construct_mesh_from_array)
 construct_mesh_from_coeffs = task(construct_mesh_from_coeffs)
 construct_mesh_from_points = task(construct_mesh_from_points)
@@ -26,4 +23,3 @@ fit_pca_model = task(fit_pca_model)
 get_shape_coefficients = task(get_shape_coefficients)
 get_shape_properties = task(get_shape_properties)
 make_voxels_array = task(make_voxels_array)
-merge_shape_modes = task(merge_shape_modes)

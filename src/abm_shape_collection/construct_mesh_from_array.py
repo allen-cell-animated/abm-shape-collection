9635b7d3bde3aa7b706d7a9743a36c4939a2d413
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-07-14 10:41:33 -0400

    Add calculate mesh centroid from reference array

--- src/abm_shape_collection/construct_mesh_from_array.py
+++ src/abm_shape_collection/construct_mesh_from_array.py
@@ -1,10 +1,20 @@
 import numpy as np
 from aicsshparam import shtools
 from vtk import vtkPolyData
+from vtk.util import numpy_support
 
 
 def construct_mesh_from_array(array: np.ndarray, reference: np.ndarray) -> vtkPolyData:
     _, angle = shtools.align_image_2d(image=reference)
+    aligned_reference = shtools.apply_image_alignment_2d(reference, angle).squeeze()
     aligned_array = shtools.apply_image_alignment_2d(array, angle).squeeze()
-    mesh, _, _ = shtools.get_mesh_from_image(image=aligned_array)
+
+    mesh, _, _ = shtools.get_mesh_from_image(
+        image=aligned_array, lcc=False, translate_to_origin=False
+    )
+
+    centroid = np.argwhere(aligned_reference == 1).mean(axis=0)[[2, 1, 0]]
+    coords = numpy_support.vtk_to_numpy(mesh.GetPoints().GetData()) - centroid
+    mesh.GetPoints().SetData(numpy_support.numpy_to_vtk(coords))
+
     return mesh

00415b921d95574c6f29a8e3e83659777f68a467
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-07-18 18:48:46 -0400

    Add docstrings and unit tests for extract mesh projections task

--- src/abm_shape_collection/extract_mesh_projections.py
+++ src/abm_shape_collection/extract_mesh_projections.py
@@ -3,13 +3,14 @@ from typing import Optional, Union
 
 import numpy as np
 import trimesh
-from vtk import vtkPLYWriter, vtkPolyData
+from vtk import vtkPLYWriter, vtkPolyData  # pylint: disable=no-name-in-module
 
 PROJECTIONS: list[tuple[str, tuple[int, int, int], int]] = [
     ("side1", (0, 1, 0), 1),
     ("side2", (1, 0, 0), 0),
     ("top", (0, 0, 1), 2),
 ]
+"""Mesh projection names, normals, and extent axes."""
 
 
 def extract_mesh_projections(
@@ -18,6 +19,31 @@ def extract_mesh_projections(
     extents: bool = True,
     offset: Optional[tuple[float, float, float]] = None,
 ) -> dict:
+    """
+    Extract slices and/or extents from mesh.
+
+    Slice projections are taken as the cross section of the mesh with planes in
+    the x, y, and z directions with origin at (0,0,0). Extent projections are
+    taken as cross section of the mesh with planes in the x, y, and z directions
+    at increments of 0.5.
+
+    Parameters
+    ----------
+    mesh
+        Mesh object.
+    slices
+        True to extract slices, False otherwise.
+    extents : bool, optional
+        True to extract extents, False otherwise.
+    offset
+        Mesh translation applied before extracting slices and/or meshes.
+
+    Returns
+    -------
+    :
+        Map of mesh projection path points.
+    """
+
     if isinstance(mesh, vtkPolyData):
         mesh = convert_vtk_to_trimesh(mesh)
 
@@ -40,6 +66,19 @@ def extract_mesh_projections(
 
 
 def convert_vtk_to_trimesh(mesh: vtkPolyData) -> trimesh.Trimesh:
+    """
+    Converts VTK polydata to trimesh object.
+
+    Parameters
+    ----------
+    mesh
+        VTK mesh object.
+
+    Returns
+    -------
+    :
+        Trimesh mesh object.
+    """
     with tempfile.NamedTemporaryFile() as temp:
         writer = vtkPLYWriter()
         writer.SetInputData(mesh)
@@ -52,7 +91,22 @@ def convert_vtk_to_trimesh(mesh: vtkPolyData) -> trimesh.Trimesh:
 
 
 def get_mesh_slice(mesh: trimesh.Trimesh, normal: tuple[int, int, int]) -> list[list[list[float]]]:
-    """Get slice of mesh along plane for given normal as path points."""
+    """
+    Get slice of mesh along plane for given normal as path points.
+
+    Parameters
+    ----------
+    mesh
+        Mesh object.
+    normal
+        Vector normal to slice plane.
+
+    Returns
+    -------
+    :
+        List of connected vertices in space specifying the slice.
+    """
+
     mesh_slice = mesh.section_multiplane((0, 0, 0), normal, [0])
     points = [[list(point) for point in entity] for entity in mesh_slice[0].discrete]
     return points
@@ -61,7 +115,24 @@ def get_mesh_slice(mesh: trimesh.Trimesh, normal: tuple[int, int, int]) -> list[
 def get_mesh_extent(
     mesh: trimesh.Trimesh, normal: tuple[int, int, int], index: int
 ) -> dict[float, list[list[list[float]]]]:
-    """Get extent of mesh along plane for given normal as path points."""
+    """
+    Get extent of mesh along plane for given normal as path points.
+
+    Parameters
+    ----------
+    mesh
+        Mesh object.
+    normal
+        Vector normal to slice plane.
+    index
+        Index of normal axis.
+
+    Returns
+    -------
+    :
+        Map to list of connected vertices in space specifying the extent.
+    """
+
     layers = int(mesh.extents[index] + 2)
     plane_indices = list(np.arange(-layers, layers + 1, 0.5))
     mesh_extents = mesh.section_multiplane((0, 0, 0), normal, plane_indices)
---
+++ tests/abm_shape_collection/test_extract_mesh_projections.py
@@ -0,0 +1,168 @@
+import unittest
+
+import trimesh
+from vtk import vtkCellArray, vtkIdList, vtkPoints, vtkPolyData
+
+from abm_shape_collection.extract_mesh_projections import (
+    PROJECTIONS,
+    convert_vtk_to_trimesh,
+    extract_mesh_projections,
+    get_mesh_extent,
+    get_mesh_slice,
+)
+
+
+class TestConstructExtractMeshProjections(unittest.TestCase):
+    def setUp(self) -> None:
+        vertices = [
+            (-1, -1, 0),
+            (1, -1, 0),
+            (-1, 1, 0),
+            (1, 1, 0),
+            (0, 0, 1),
+            (0, 0, -1),
+        ]
+
+        faces = [
+            (0, 1, 4),
+            (1, 3, 4),
+            (3, 2, 4),
+            (2, 0, 4),
+            (0, 1, 5),
+            (1, 3, 5),
+            (3, 2, 5),
+            (2, 0, 5),
+        ]
+
+        self.vertices = vertices
+        self.faces = faces
+
+        offset_vertices = [
+            (-1.5, -1, 0),
+            (0.5, -1, 0),
+            (-1.5, 1, 0),
+            (0.5, 1, 0),
+            (-0.5, 0, 1),
+            (-0.5, 0, -1),
+        ]
+
+        vtk_mesh = vtkPolyData()
+        points = vtkPoints()
+        polys = vtkCellArray()
+
+        for index, vertex in enumerate(vertices):
+            points.InsertPoint(index, vertex)
+
+        for face in faces:
+            id_list = vtkIdList()
+            for index in face:
+                id_list.InsertNextId(index)
+            polys.InsertNextCell(id_list)
+
+        vtk_mesh.SetPoints(points)
+        vtk_mesh.SetPolys(polys)
+
+        tri_mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
+        tri_mesh_offset = trimesh.Trimesh(vertices=offset_vertices, faces=faces)
+
+        self.vtk_mesh = vtk_mesh
+        self.tri_mesh = tri_mesh
+        self.tri_mesh_offset = tri_mesh_offset
+
+        top_full_square = [[-1, 1], [-1, -1], [1, -1], [1, 1], [-1, 1]]
+        top_half_square = [
+            [-0.5, 0.5],
+            [-0.5, -0.5],
+            [0.5, -0.5],
+            [0.5, 0.5],
+            [-0.5, 0.5],
+        ]
+
+        side_full_diamond = [[0, -1], [1, 0], [0, 1], [-1, 0], [0, -1]]
+        side_half_diamond = [
+            [0, -1],
+            [0.5, -0.5],
+            [0.5, 0.5],
+            [0, 1],
+            [-0.5, 0.5],
+            [-0.5, -0.5],
+            [0, -1],
+        ]
+
+        self.slices = {
+            "top": [top_full_square],
+            "side1": [side_full_diamond],
+            "side2": [side_full_diamond],
+        }
+
+        self.extents = {
+            "top": {
+                -0.5: [top_half_square],
+                0: [top_full_square],
+                0.5: [top_half_square],
+            },
+            "side1": {
+                -1: [],
+                -0.5: [side_half_diamond],
+                0: [side_full_diamond],
+                0.5: [side_half_diamond],
+            },
+            "side2": {
+                -1: [],
+                -0.5: [side_half_diamond],
+                0: [side_full_diamond],
+                0.5: [side_half_diamond],
+            },
+        }
+
+    def test_extract_extract_mesh_projections_slices_no_translation(self):
+        projections = extract_mesh_projections(
+            self.vtk_mesh, slices=True, extents=False, offset=None
+        )
+        for proj, _, _ in PROJECTIONS:
+            self.assertCountEqual(self.slices[proj], projections[f"{proj}_slice"])
+            self.assertFalse(f"{proj}_extent" in proj)
+
+    def test_extract_extract_mesh_projections_extents_no_translation(self):
+        projections = extract_mesh_projections(
+            self.vtk_mesh, slices=False, extents=True, offset=None
+        )
+        for proj, _, _ in PROJECTIONS:
+            self.assertDictEqual(self.extents[proj], projections[f"{proj}_extent"])
+            self.assertFalse(f"{proj}_slice" in proj)
+
+    def test_extract_extract_mesh_projections_with_translation(self):
+        offset = (0.5, 0, 0)
+        projections = extract_mesh_projections(
+            self.tri_mesh_offset, slices=True, extents=True, offset=offset
+        )
+        for proj, _, _ in PROJECTIONS:
+            self.assertCountEqual(self.slices[proj], projections[f"{proj}_slice"])
+            self.assertDictEqual(self.extents[proj], projections[f"{proj}_extent"])
+
+    def test_convert_vtk_to_trimesh(self):
+        tri_mesh = convert_vtk_to_trimesh(self.vtk_mesh)
+        self.assertTrue((self.tri_mesh.vertices == tri_mesh.vertices).all())
+        self.assertTrue((self.tri_mesh.faces == tri_mesh.faces).all())
+
+    def test_get_mesh_slice(self):
+        top_points = get_mesh_slice(self.tri_mesh, (0, 0, 1))
+        side1_points = get_mesh_slice(self.tri_mesh, (1, 0, 0))
+        side2_points = get_mesh_slice(self.tri_mesh, (0, 1, 0))
+
+        self.assertCountEqual(self.slices["top"], top_points)
+        self.assertCountEqual(self.slices["side1"], side1_points)
+        self.assertCountEqual(self.slices["side2"], side2_points)
+
+    def test_get_mesh_extent(self):
+        top_points = get_mesh_extent(self.tri_mesh, (0, 0, 1), 2)
+        side1_points = get_mesh_extent(self.tri_mesh, (1, 0, 0), 1)
+        side2_points = get_mesh_extent(self.tri_mesh, (0, 1, 0), 0)
+
+        self.assertDictEqual(self.extents["top"], top_points)
+        self.assertDictEqual(self.extents["side1"], side1_points)
+        self.assertDictEqual(self.extents["side2"], side2_points)
+
+
+if __name__ == "__main__":
+    unittest.main()

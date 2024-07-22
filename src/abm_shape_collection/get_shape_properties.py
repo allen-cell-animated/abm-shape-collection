f335be37e0796918cffad7f9389f05c1a6d1a3e8
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-07-22 16:58:28 -0400

    Add docstrings and unit test for get shape properties task

--- src/abm_shape_collection/get_shape_properties.py
+++ src/abm_shape_collection/get_shape_properties.py
@@ -3,6 +3,25 @@ from skimage.measure import regionprops_table
 
 
 def get_shape_properties(array: np.ndarray, properties: list[str]) -> dict:
+    """
+    Calculate shape properties for binary image array.
+
+    Shape properties must be value properties from skimage.measure.regionprops.
+    Image is projected along the first axis.
+
+    Parameters
+    ----------
+    array
+        Binary image array.
+    properties
+        List of shape properties to calculate.
+
+    Returns
+    -------
+    :
+        Dictionary of calculated shape properties.
+    """
+
     flattened_array = array.max(axis=0)
 
     properties_dict = regionprops_table(flattened_array, properties=properties)

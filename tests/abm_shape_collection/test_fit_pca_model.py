cf209de2801f52641e36c5b7675b1e28b0fac574
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-07-22 15:10:14 -0400

    Add docstrings and unit test for fit pca model task

---
+++ tests/abm_shape_collection/test_fit_pca_model.py
@@ -0,0 +1,27 @@
+import unittest
+
+import numpy as np
+
+from abm_shape_collection.fit_pca_model import fit_pca_model
+
+
+class TestFitPCAModel(unittest.TestCase):
+    def test_fit_pca_model(self):
+        components = 2
+        num_features = 5
+        num_samples = 100
+
+        features = np.random.random_sample((num_samples, num_features))
+        ordering = np.random.random_sample((num_samples,))
+
+        # Ensure that at least one of the features needs to be reoriented.
+        features[:, 0] = ordering * -10
+
+        pca = fit_pca_model(features, components, ordering)
+
+        self.assertEqual(components, pca.n_components_)
+        self.assertEqual(num_samples, pca.n_samples_)
+
+
+if __name__ == "__main__":
+    unittest.main()
